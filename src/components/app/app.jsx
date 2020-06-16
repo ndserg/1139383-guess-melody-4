13582efa24725b547c7ece5936b60e88fcaf358f
ndserg <ndserg@gmail.com>
2020-06-16 21:23:39 +0300

    2.1 Установит prop-types и добавит валидацию пропсов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import PropTypes from "prop-types";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
+
   const {errorsCount} = props;
 
   return (
@@ -10,5 +11,8 @@ const App = (props) => {
   );
 };
 
+App.propTypes = {
+  errorsCount: PropTypes.number.isRequired,
+};
 
 export default App;
